f01f5b34f26e8d49c0992e7eff9175b5aea81db2
Zhang张政明 <zhang.zhengming@immomo.com>
2015-12-22 10:42:34 +0800

    dnd init

---
+++ src/dnd/DnDManager.js
@@ -0,0 +1,27 @@
+var core = require('../../core'),
+    DisplayObject = core.DisplayObject;
+
+var dndManager;
+
+Object.defineProperties
+
+function DnDManager(stage) {
+    dndManager = this;
+    this._stage = stage;
+}
+
+DnDManager.prototype.addSource = function(source) {
+
+};
+
+DnDManager.prototype.removeSource = function(source) {
+
+};
+
+DnDManager.prototype.addTarget = function(target) {
+
+};
+
+DnDManager.prototype.removeTarget = function(target) {
+
+};
---
+++ src/touch/dnd/DnDManager.js
@@ -0,0 +1,13 @@
+
+
+function DnDManager(stage) {
+    this._stage = stage;
+}
+
+DnDManager.prototype.addSource = function(source) {
+
+};
+
+DnDManager.prototype.removeSource = function(source) {
+
+};
