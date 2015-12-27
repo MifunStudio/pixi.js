54c577b6563c4ead9cc8fa25ef609c9fedbb3041
mifun studio <mifunstudio@zhangzhengmingdeMacBook-Pro.local>
2015-12-27 23:45:22 +0800

    delete game

--- game/boot.js
+++
@@ -1,2 +0,0 @@
-document.write('<script src="../bin/pixi.js"></script>');
-document.write('<script src="dist/mifun-game.js"></script>');
--- game/tools/datagrep/client/app/view/main/ViewportModel.js
+++
@@ -1,8 +0,0 @@
-Ext.define('datagrep.view.main.ViewportModel', {
-    extend: 'Ext.app.ViewModel',
-    alias: 'viewmodel.mainviewport',
-
-    data: {
-        currentView: null
-    }
-});
