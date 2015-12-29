0c19647837ced3b25bd3ea3b5e2f99cc36d9b570
mifun studio <mifunstudio@zhangzhengmingdeMacBook-Pro.local>
2015-12-29 22:55:28 +0800

    update: TweenTransform wrapper instead of this

--- src/extras/tween.js
+++ src/extras/tween.js
@@ -9,13 +9,80 @@ var genTweenId = (function() {
     }
 })();
 
+function TweenTransform(displayObject) {
+    this.displayObject = displayObject;
+}
+Object.defineProperties(TweenTransform.prototype, {
+    x: {
+        get: function() {
+            return this.displayObject.x;
+        },
+        set: function(value) {
+            this.displayObject.x = value;
+        }
+    },
+    y: {
+        get: function() {
+            return this.displayObject.y;
+        },
+        set: function(value) {
+            this.displayObject.y = value;
+        }
+    },
+    scaleX: {
+        get: function() {
+            return this.displayObject.scaleX;
+        },
+        set: function(value) {
+            this.displayObject.scaleX = value;
+        }
+    },
+    scaleY: {
+        get: function() {
+            return this.displayObject.scaleY;
+        },
+        set: function(value) {
+            this.displayObject.scaleY = value;
+        }
+    },
+    alpha: {
+        get: function() {
+            return this.displayObject.alpha;
+        },
+        set: function(value) {
+            this.displayObject.alpha = value;
+        }
+    },
+    rotation: {
+        get: function() {
+            return this.displayObject.rotation;
+        },
+        set: function(value) {
+            this.displayObject.rotation = value;
+        }
+    }
+});
+
+DisplayObject.prototype._tweenTransform = null;
+
+Object.defineProperties(DisplayObject.prototype, {
+    tweenTransform: {
+        get: function() {
+            if(!this._tweenTransform) {
+                this._tweenTransform = new TweenTransform(this);
+            }
+            return this._tweenTransform;
+        }
+    }
+});
+
 DisplayObject.prototype.tween = function(clearTweens) {
     var tween;
     if(clearTweens) {
         this.clearTweens();
     }
     this._tweens = this._tweens || {};
-    tween = new TWEEN.Tween(this);
+    tween = new TWEEN.Tween(this.tweenTransform);
     tween.start();
     this._tweens[genTweenId()] = tween;
     return tween;
